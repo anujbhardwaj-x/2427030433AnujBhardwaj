73c8aaff02da4e0a323844e28d93a66490a1cdb3
Anuj Bhardwaj <anujbxd@gmail.com>
2025-10-29 11:40:34 +0530

    Create selection.py

---
+++ Lab9/selection.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+numList = [8, 7, 13, 1, -9, 4]
+selection_sort(numList)
+print(numList)
